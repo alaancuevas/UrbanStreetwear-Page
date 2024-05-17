139945ebc9ffbcb540facdb6aa2b067ba1e6c1af
Pablo Damián Alcaraz <pablodalcaraz@gmail.com>
2024-05-17 15:23:08 -0300

    agrego funciones carrito de compras

---
+++ carrito/carrito.js
@@ -0,0 +1,2 @@
+const carritoDeCompras = document.getElementById('carrito');
+carritoDeCompras.
--- productos/functions.js
+++ productos/functions.js
@@ -1,122 +1,143 @@
 const productos = [
     {
+        idProducto : 1,
         imagen: "/product-images/remeraOversize.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Remera Oversize",
         precio: "$////"
     },
     {
+        idProducto : 2,
         imagen: "/product-images/jeanMom.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "*Jeans* Mom",
         precio: "$////"
     },
     {
+        idProducto : 3,
         imagen: "/product-images/canguroOversize.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Hoodie Oversize",
         precio: "$////"
     },
     {
+        idProducto : 4,
         imagen: "/product-images/conjuntoSyR.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Short & Remera",
         precio: "$////"
     },
     {
+        idProducto : 5,
         imagen: "/product-images/gorras.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Gorras planas",
         precio: "$////"
     }, {
+        idProducto:6,
         imagen: "/product-images/campera.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Campera inflable",
         precio: "$////"
     },
     {
+        idProducto: 7,
         imagen: "/product-images/zapatillas.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Zapatillas Air Jordan",
         precio: "$////"
     },
     {
+        idProducto: 8,
         imagen: "/product-images/joggerCargo.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Jogger Cargo",
         precio: "$////"
     },
     {
+        idProducto: 9,
         imagen: "/product-images/bermudajersey.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Bermuda Jersey",
         precio: "$////"
     },
     {
+        idProducto: 10,
         imagen: "/product-images/camisetaBaseball.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Camiseta NY Yankees",
         precio: "$////"
     }, {
+        idProducto: 11,
         imagen: "/product-images/buzo.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Buzo c/redondo",
         precio: "$////"
     },
     {
+        idProducto: 12,
         imagen: "/product-images/jeanSlim.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Jeans Slim Fit",
         precio: "$////"
     },
     {
+        idProducto: 13,
         imagen: "/product-images/jeanCargo.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Jeans Cargo",
         precio: "$////"
     },
     {
+        idProducto: 14,
         imagen: "/product-images/remeraOso.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Remera Bear OVSZ",
         precio: "$////"
     },
     {
+        idProducto: 15,
         imagen: "/product-images/morral.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Morral Basic",
         precio: "$////"
     }, {
+        idProducto: 16,
         imagen: "/product-images/pescador.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Piluso Pescador",
         precio: "$////"
     },
     {
+        idProducto: 17,
         imagen: "/product-images/vans.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Vans SK8-HI",
         precio: "$////"
     },
     {
+        idProducto: 18,
         imagen: "/product-images/camuflado.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Jogger Camuflado",
         precio: "$////"
     },
     {
+        idProducto: 19,
         imagen: "/product-images/gorro.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Gorro Billabong",
         precio: "$////"
     },
     {
+        idProducto: 20,
         imagen: "/product-images/champion.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Remera Oversize",
         precio: "$////"
     },
     {
+        idProducto: 21,
         imagen: "/product-images/universitaria.jpg",
         logo: "/images/urbanLogoNegro.png",
         nombre: "Campera Universitaria",
@@ -145,12 +166,37 @@ function mostrar(pagina) {
                     <img src="${producto.logo}" alt="logo urban">
                     <h2>${producto.nombre}</h2>
                     <h3>${producto.precio}</h3>
-                    <div class="agregar"><button>Agregar al carrito</button></div>
+                    <div class="agregar"><button class="agregar-al-carrito">Agregar al carrito</button></div>
+                    <div class="eliminar"><button class="eliminar-delcarrito">Eliminar</button></div>
                 </div>
             </div>
         `;
         contenedorProductos.insertAdjacentHTML('beforeend', prodHTML);
     });
+    const agregar = document.querySelectorAll('.agregar-al-carrito');
+    
+     agregar.forEach(boton =>{
+       
+       boton.addEventListener('click', (ev) =>{
+       incrementarContador();
+       document.querySelector('.shopping-cart .count p').innerText= contador;
+       document.querySelector('.eliminar').style.display="flex";
+       boton.innerText='Producto agregado!'
+       boton.agregado=true;
+       ev.target.style.backgroundColor = '#008f39';
+       
+    
+    });
+});
+};
+let contador=0;
+function incrementarContador(){
+    contador ++;
+}
+
+
+function agregarAlCarrito(productId){
+    let carrito=JSON.parse(local)
 }
 
 function siguiente() {
@@ -182,4 +228,6 @@ document.querySelector('.siguiente').addEventListener('click', siguiente);
 
 mostrar(paginaVista);
 actualizarBotones();
+
+
 
